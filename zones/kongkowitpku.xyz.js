6dd1020e7f799e3bef598792deccd63b8dffebfc
Little Human <little.human@novonine.space>
2022-09-10 12:34:39 +0700

    Formating
    Signed-off-by: Little Human <little.human@novonine.space>

--- zones/kongkowitpku.xyz.js
+++ zones/kongkowitpku.xyz.js
@@ -5,7 +5,7 @@ D(
 
     // Root
     ALIAS("@", "kongkow-soon.pages.dev.", CF_PROXY_ON),
-    CNAME('www', '@', CF_PROXY_ON),
+    CNAME("www", "@", CF_PROXY_ON),
 
     // Redirects
     CF_REDIRECT("www.kongkowitpku.xyz/*", "https://kongkowitpku.xyz/$1")
