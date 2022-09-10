43ee9a944a2b150cc251d2e690cec2d847ad7616
Little Human <little.human@novonine.space>
2022-09-10 12:30:38 +0700

    Redirect www to root
    Signed-off-by: Little Human <little.human@novonine.space>

--- zones/kongkowitpku.xyz.js
+++ zones/kongkowitpku.xyz.js
@@ -5,5 +5,8 @@ D(
 
     // Root
     ALIAS("@", "kongkow-soon.pages.dev.", CF_PROXY_ON),
-    A("www", "192.0.2.1", CF_PROXY_ON)
+    A("www", "192.0.2.1", CF_PROXY_ON),
+
+    // Redirects
+    CF_REDIRECT("www.kongkowitpku.xyz/*", "https://kongkowitpku.xyz/$1")
 );
